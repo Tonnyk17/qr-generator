7baf0188211fed248a962a3c4e13877d82ab140d
Tony <tonykurosaki117@gmail.com>
2022-01-24 01:57:29 -0600

    v1 finished

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -19,7 +19,7 @@ export const Forms = () => {
     useEffect(() => {
         const dbRef = ref(getDatabase());
         get(child(dbRef, 'users/')).then(async(snapshot) => {
-            if(snapshot.size > 20){  
+            if(snapshot.size > 150){  
                 setIsLimited(true);  
             }
         })
